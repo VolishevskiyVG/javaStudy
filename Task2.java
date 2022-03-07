006029f9be4e75aed661d3baabe8637c65c6a8c8
Vasiliy Volishevskiy <Volishevski.V@gmail.com>
2022-03-07 15:13:53 +0300

    load HW

---
+++ Task2.java
@@ -0,0 +1,15 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 4;
+        System.out.println(a*b);
+        System.out.println(a-b);
+        System.out.println(a+b);
+        System.out.println(1.0* a/b);
+        System.out.println(a%b);
+        System.out.println("a is odd: " + a%2);
+        System.out.println("b is add: " + b%2);
+
+
+        System.out.println("\uD83D\uDE00");    }
+}
